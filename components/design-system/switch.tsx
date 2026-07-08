cabee282a6d4aef94cc5a2ace56875ef9bde58ec
MOCHIZUKI Natsune <me@natsuneko.cat>
2026-07-08 19:15:20 +0900

    fix: use disabled property

--- components/design-system/switch.tsx
+++ components/design-system/switch.tsx
@@ -1,21 +1,19 @@
 import { cn } from "@/lib/utils";
-import { Switch, View } from "react-native";
+import { Switch } from "react-native";
 
 export type CatalystSwitchProps = React.ComponentProps<typeof Switch> & {
   className?: string;
 };
 
-export const CatalystSwitch = ({ className, disabled, ...props }: CatalystSwitchProps) => {
+export const CatalystSwitch = ({ className, ...props }: CatalystSwitchProps) => {
   return (
-    <View className={cn("shrink-0", disabled && "opacity-50", className)}>
-      <Switch
-        disabled={disabled}
-        thumbColorClassName="accent-light-background dark:accent-dark-text"
-        trackColorOffClassName="accent-light-border-strong dark:accent-dark-border-strong"
-        trackColorOnClassName="accent-light-tint dark:accent-dark-accent"
-        ios_backgroundColorClassName="accent-light-border-strong dark:accent-dark-border-strong"
-        {...props}
-      />
-    </View>
+    <Switch
+      className={cn("shrink-0 disabled:opacity-50", className)}
+      thumbColorClassName="accent-light-background dark:accent-dark-text"
+      trackColorOffClassName="accent-light-border-strong dark:accent-dark-border-strong"
+      trackColorOnClassName="accent-light-tint dark:accent-dark-accent"
+      ios_backgroundColorClassName="accent-light-border-strong dark:accent-dark-border-strong"
+      {...props}
+    />
   );
 };
